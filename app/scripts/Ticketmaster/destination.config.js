a40b73b116dfa8548d3036e8f2e2af54da1c7f70
stephen <stephen.freir@inflightdublin.com>
2016-08-17 00:08:03 +0100

    structure

--- app/scripts/Ticketmaster/destination.config.js
+++ app/scripts/Ticketmaster/destination.config.js
@@ -1,16 +1,16 @@
 (function () {
     'use strict';
     angular.module('inflight.destination', [
-        'inflight.destination.ticketmaster',
-        'inflight.destination.cityhook',
-        'inflight.destination.cityguide',
-        // 'likewhere.destinations',
-        // 'inflight.destination.livingsocial',
-        'inflight.destination.groupon',
-        'inflight.destination.hotel',
-        'inflight.destination.car',
-        'inflight.destination.advertisement',
-        'inflight.destination.information'
+        'inflight.destination.ticketmaster'
+//        'inflight.destination.cityhook',
+//        'inflight.destination.cityguide',
+//        // 'likewhere.destinations',
+//        // 'inflight.destination.livingsocial',
+//        'inflight.destination.groupon',
+//        'inflight.destination.hotel',
+//        'inflight.destination.car',
+//        'inflight.destination.advertisement',
+//        'inflight.destination.information'
     ])
         .config(config);
 
